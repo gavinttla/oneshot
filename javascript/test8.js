06c2cd2044c37d28c540aa0501d57ccbb4aebcba
Gavin B. Cui <GBCui@dm.local>
2017-11-02 15:33:20 -0700

    test8

--- javascript/test8.js
+++ javascript/test8.js
@@ -3,6 +3,27 @@ function msgAfterTimeout (msg, who, timeout) {
         setTimeout(() => resolve(`${msg} Hello ${who}!`), timeout)
     })
 }
+
+
+msgAfterTimeout("", "Foo", 1000).then((msg) =>{
+    return msgAfterTimeout(msg, "Bar", 2000)
+}).then((msg) => {
+    console.log(`done after 2000ms:${msg}`)
+    return msgAfterTimeout(msg, "puff", 1000)
+}).then((msg) => {
+    console.log(`done again after 2000ms:${msg}`)
+})
+
+
+
+/*
+var mypo = msgAfterTimeout("", "Foo", 500)
+
+mypo.then((msg) => {
+    console.log(`done after 500ms:${msg}`)
+})
+*/
+/*
 msgAfterTimeout("", "Foo", 1000).then((msg) =>
     msgAfterTimeout(msg, "Bar", 2000)
 ).then((msg) => {
@@ -11,4 +32,4 @@ msgAfterTimeout("", "Foo", 1000).then((msg) =>
 }).then((msg) => {
     console.log(`done again after 2000ms:${msg}`)
 })
-
+*/
